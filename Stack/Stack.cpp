bf316d7e4e7e32d16cb8ebd379c2f5e0cc0039a3
张鹏宇 <1055803945@qq.com>
2023-07-24 16:03:17 +0800

    更新Stack

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,21 +1,56 @@
 #include<iostream>
-#include "Data.cpp"
+#include "../Array/Array.cpp"
+#include<stdexcept>
 
 using namespace std;
 
 template<typename T>
-class Stack
+class Stack: public Array<T>
 {
-private:
-    Data<T>* data;
-    int size;
-    int top;
 public:
-    Stack();
-    ~Stack();
-    bool empty();
     T top();
     void push(T data);
     void pop();
+    // bool empty();
+    // int size();
+    Stack();
+    ~Stack();
 };
 
+template<typename T>
+T Stack<T>::top()
+{
+    if (this->getsize() == 0)
+    {
+        throw out_of_range("Stack is empty");
+    }
+    return this->get(this->getsize()-1);
+}
+
+template<typename T>
+void Stack<T>::push(T data)
+{
+    this->push_back(data);
+}
+
+template<typename T>
+void Stack<T>::pop()
+{
+    this->pop_back();
+}
+
+// template<typename T>
+// int Stack<T>::size()
+// {
+//     return this->size;
+// }
+
+template<typename T>
+Stack<T>::Stack()
+{
+}
+
+template<typename T>
+Stack<T>::~Stack()
+{
+}
